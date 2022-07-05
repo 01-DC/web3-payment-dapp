5cf7fa79cb03f5cf7329ca2e6ed514c4b9b7312f
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-07-05 13:25:56 +0530

    finished landing page

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from "react"
 
 const Loader = () => {
-  return (
-    <div>Loader</div>
-  )
+	return (
+		<div className="flex justify-center items-center py-3">
+			<div className="animate-spin rounded-full h-28 w-28 border-b-2 border-red-700" />
+		</div>
+	)
 }
 
 export default Loader
