db4d59d6fecc3967c814450618eeee3f3df9a74f
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-07-10 23:14:14 +0530

    created hardhat config file

--- smart_contracts/hardhat.config.js
+++ smart_contracts/hardhat.config.js
@@ -1,8 +1,16 @@
 // https://eth-goerli.g.alchemy.com/v2/tKOeCTAxZrE-Af2Gf_FYZbj9V8ZBiuZU
 
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomiclabs/hardhat-waffle")
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.9",
-};
+	solidity: "0.8.0",
+	networks: {
+		goerli: {
+			url: "https://eth-goerli.g.alchemy.com/v2/tKOeCTAxZrE-Af2Gf_FYZbj9V8ZBiuZU",
+			accounts: [
+				"0b86e6f9fd54b5304aede1cca35b4def77dd27ee1b4616f8133d3fdf320f30c7",
+			],
+		},
+	},
+}
