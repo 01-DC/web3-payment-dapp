f97e225176807080af0b27be5d886c345a8ef675
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-07-10 23:57:56 +0530

    created Context for connecting wallet & handling no metamask cases

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useContext } from "react"
 
 import { AiFillPlayCircle } from "react-icons/ai"
 import { SiEthereum } from "react-icons/si"
 import { BsInfoCircle } from "react-icons/bs"
 
 import { Loader } from "."
+
+import { TransactionContext } from "../context/TransactionContext"
 
 const commonStyles =
 	"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
@@ -21,8 +23,10 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 )
 
 const Welcome = () => {
-	const connectWallet = () => {}
+	const { connectWallet, currentAccount } = useContext(TransactionContext)
+
 	const handleSubmit = () => {}
+
 	return (
 		<div className="flex w-full justify-center items-center ">
 			<div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -35,14 +39,16 @@ const Welcome = () => {
 						Explore the crypto world. Buy and sell cryptocurrencies
 						on Krypt
 					</p>
-					<button
-						type="button"
-						onClick={connectWallet}
-						className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
-						<p className="text-white text-base font-semibold">
-							Connect Wallet
-						</p>
-					</button>
+					{!currentAccount && (
+						<button
+							type="button"
+							onClick={connectWallet}
+							className="flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]">
+							<p className="text-white text-base font-semibold">
+								Connect Wallet
+							</p>
+						</button>
+					)}
 
 					<div className="grid sm:grid-cols-3 grid-cols-2 w-full mt-10">
 						<div className={`rounded-tl-2xl ${commonStyles}`}>
--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,13 @@ import React from "react"
 import ReactDOM from "react-dom/client"
 
 import "./index.css"
+import { TransactionProvider } from "./context/TransactionContext"
 import App from "./App"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<TransactionProvider>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</TransactionProvider>
 )
