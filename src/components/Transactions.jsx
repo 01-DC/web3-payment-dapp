f86a1ef3f5c23e499230765ac8717b65635a8163
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-07-16 01:20:35 +0530

    imported dummy transaction data & updated Welcome component

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
+import React, { useContext } from "react"
+
+import { TransactionContext } from "../context/TransactionContext"
+
+import dummyData from "../utils/dummyData"
+import { shortenAddress } from "../utils/shortenAddress"
+
+const TransactionCard = ({
+	addressTo,
+	addressFrom,
+	timestamp,
+	message,
+	keyword,
+	amount,
+	url,
+}) => {
+	return "transaction"
+}
 
 const Transactions = () => {
-  return (
-    <div>Transactions</div>
-  )
+	const { currentAccount } = useContext(TransactionContext)
+	return (
+		<div className="flex w-full justify-center items-center 2xl:p-20 gradient-bg-transactions">
+			<div className="flex flex-col md:p-12 py-12 px-4">
+				{currentAccount ? (
+					<h3 className="text-white text-3xl text-center my-2">
+						Latest Transactions
+					</h3>
+				) : (
+					<h3 className="text-white text-3xl text-center my-2">
+						Connect your account to see the latest transactions
+					</h3>
+				)}
+
+				<div className="flex flex-wrap justify-center items-center mt-10">
+					{dummyData.reverse().map((transaction, index) => (
+						<TransactionCard key={index} {...transaction} />
+					))}
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default Transactions
--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -7,6 +7,7 @@ import { BsInfoCircle } from "react-icons/bs"
 import { Loader } from "."
 
 import { TransactionContext } from "../context/TransactionContext"
+import { shortenAddress } from "../utils/shortenAddress"
 
 const commonStyles =
 	"min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white"
@@ -91,7 +92,9 @@ const Welcome = () => {
 							</div>
 							<div className="">
 								<p className="text-white font-light text-sm ">
-									Address
+									{currentAccount
+										? shortenAddress(currentAccount)
+										: "Disconnected"}
 								</p>
 								<p className="text-white font-semibold text-lg mt-1">
 									Ethereum
