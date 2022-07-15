8da4bd5fb06a86c0c07a6d1b789af05fb98c1997
Devanshu Chatterjee <devanshu4190@gmail.com>
2022-07-16 05:01:38 +0530

    created transaction history cards

--- src/components/Transactions.jsx
+++ src/components/Transactions.jsx
@@ -14,7 +14,42 @@ const TransactionCard = ({
 	amount,
 	url,
 }) => {
-	return "transaction"
+	return (
+		<div className="bg-[#181918] m-4 flex flex-1 2xl:min-w-[450px] 2xl:max-w-[500px] sm:min-w-[270px] sm:max-w-[300px] flex-col p-3 rounded-md hover:shadow-2xl">
+			<div className="flex flex-col items-center w-full mt-3 ">
+				<div className="w-full mb-6 p-2">
+					<a
+						href={`https://goerli.etherscan.io/address/${addressFrom}`}
+						target="_blank"
+						rel="noopener noreferrer">
+						<p className="text-white text-base">
+							From: {shortenAddress(addressFrom)}
+						</p>
+					</a>
+					<a
+						href={`https://goerli.etherscan.io/address/${addressTo}`}
+						target="_blank"
+						rel="noopener noreferrer">
+						<p className="text-white text-base">
+							To: {shortenAddress(addressTo)}
+						</p>
+					</a>
+					<p className="text-white text-base">Amount: {amount}</p>
+					{message && (
+						<>
+							<br />
+							<p className="text-white text-base">
+								Message: {message}
+							</p>
+						</>
+					)}
+				</div>
+				<div className="bg-black p-3 px-5 w-max rounded-3xl -mt-5 shadow-2xl">
+					<p className="text-[#37c7da] font-bold">{timestamp}</p>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 const Transactions = () => {
